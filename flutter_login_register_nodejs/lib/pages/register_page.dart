382cb3b22caaa686f0129744fc0b032407cd1918
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-29 21:40:49 +0530

    Implement registration page UI with form, progress indicator, and input fields for username, password, and email

--- flutter_login_register_nodejs/lib/pages/register_page.dart
+++ flutter_login_register_nodejs/lib/pages/register_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
+import 'package:snippet_coder_utils/hex_color.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -8,8 +10,27 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  bool isApiCallProcess = false;
+  bool hidePassword = true;
+  static final GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+  String? userName;
+  String? password;
+  String? email;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: HexColor("#283B71"),
+        body: ProgressHUD(
+          child: Form(key: globalFormKey, child: _registerUI(context)),
+          inAsyncCall: isApiCallProcess,
+          opacity: 0.3,
+          key: UniqueKey(),
+        ),
+      ),
+    );
   }
+
+  Widget _registerUI(BuildContext context) {}
 }
