60f43f71b737b657dc22b13754eac2aee0caa9a4
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-06-17 11:32:55 +0530

    Refactor login and registration pages to improve UI consistency, enhance validation methods, and optimize database integration with sqflite_darwin plugin.

---
+++ flutter_login_register_nodejs/lib/models/login_request_model.dart
@@ -0,0 +1,18 @@
+class LoginRequestModel {
+  LoginRequestModel({required this.username, required this.password});
+
+  late final String username;
+  late final String password;
+
+  LoginRequestModel.fromJson(Map<String, dynamic> json) {
+    username = json['username'];
+    password = json['password'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['username'] = this.username;
+    _data['password'] = this.password;
+    return _data;
+  }
+}
