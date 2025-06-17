355b103af1889c23457ce8f05ef82c5d168565b0
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-06-17 12:00:53 +0530

    Add RegisterRequestModel class for handling registration data with JSON serialization

---
+++ flutter_login_register_nodejs/lib/models/register_request_model.dart
@@ -0,0 +1,25 @@
+class RegisterRequestModel {
+  RegisterRequestModel({
+    required this.username,
+    required this.password,
+    required this.email,
+  });
+
+  late final String username;
+  late final String password;
+  late final String email;
+
+  RegisterRequestModel.fromJson(Map<String, dynamic> json) {
+    username = json['username'];
+    password = json['password'];
+    email = json['email'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['username'] = this.username;
+    _data['password'] = this.password;
+    _data['email'] = this.email;
+    return _data;
+  }
+}
