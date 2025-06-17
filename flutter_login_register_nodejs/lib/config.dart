999dddb0a0504763f0b4a5ca12f63bcb83761e4f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-06-17 12:08:20 +0530

    Implement RegisterResponseModel class with JSON serialization methods and enhance LoginResponseModel for improved data handling.

---
+++ flutter_login_register_nodejs/lib/config.dart
@@ -0,0 +1,7 @@
+class Config {
+  static const String appName = 'Login Register NodeJS';
+  static const String apiUrl = 'http://localhost:3000/api';
+  static const String loginApi = '/users/login';
+  static const String registerApi = '/users/register';
+  static const String userProfileApi = '/users/user-profile';
+}
