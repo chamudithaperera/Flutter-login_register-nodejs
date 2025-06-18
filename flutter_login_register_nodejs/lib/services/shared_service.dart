7f32f33854e80f53af6a22ce3e002d5580adc78c
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-06-18 11:27:58 +0530

    Implement SharedService with methods to check login status, retrieve, and store login details using local API cache

---
+++ flutter_login_register_nodejs/lib/services/shared_service.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:api_cache_manager/api_cache_manager.dart';
+import 'package:api_cache_manager/models/cache_db_model.dart';
+import 'package:flutter_login_register_nodejs/models/login_response_model.dart';
+
+class SharedService {
+  static Future<bool> isLoggedIn() async {
+    var isKeyExists = await APICacheManager().isAPICacheKeyExist(
+      "login_details",
+    );
+    return isKeyExists;
+  }
+
+  static Future<LoginResponseModel?> logindetails() async {
+    var isKeyExists = await APICacheManager().isAPICacheKeyExist(
+      "login_details",
+    );
+
+    if (isKeyExists) {
+      var cacheData = await APICacheManager().getCacheData("login_details");
+      return loginResponseJson(cacheData.syncData);
+    }
+  }
+
+  static Future<void> setLoginDetails(LoginResponseModel model) async {
+    APICacheDBModel cacheModel = APICacheDBModel(
+      key: "login_details",
+      syncData: jsonEncode(model.toJson()),
+    );
+    await APICacheManager().addCacheData(cacheModel);
+  }
+}
